981cead4cc023cff16f7c47460095bc3d61e74f0
Joe Pettit <jpettit@pettitservers.com>
2015-02-20 22:28:11 -0600

    Oh gee, this is a really long commit message, I sure hope it doens't exceed the 255 character tweet limit

--- tweeter.py
+++ tweeter.py
@@ -2,6 +2,7 @@ from flask import *
 from config import *
 from hashlib import sha1
 
+import requests
 import json
 import hmac
 
@@ -12,17 +13,48 @@ def verify_secret(request, data):
     if sha != 'sha1':
         abort(403)
     res = hmac.new(secret, msg=data, digestmod=sha1)
-    if not hmac.compare_digest(res.hexdigest(), signature):
+    if res.hexdigest() != signature:
         return(403)
+
+def shorten(url):
+    response = requests.post('https://ptpb.pw/u', data={'c':url})
+    shortened_url = response.headers.get('Location')
+    return shortened_url
 
 @app.route('/', methods=['GET','POST'])
 def primary():
     if request.method == "GET":
         return "I'm alive."
     elif request.method == "POST":
+        verify_secret(request, request.data)
+
         data = json.loads(request.data)
-        verify_secret(request, data)
-        print "New commit by %s, %s" % (str(data['commits'][0]['author']['username']), str(data['commits'][0]['message']))
+
+        commit_message = str(data['head_commit']['message'])
+        commit_user = str(data['head_commit']['author']['username'])
+        commit_url = str(data['head_commit']['url'])
+        repo_name = str(data['repository']['name'])
+        
+        shortened_url = shorten(commit_url)
+
+        message = "New commit by %s on %s: %s %s" % (commit_user,
+                                                        repo_name,
+                                                        commit_message,
+                                                        shortened_url)
+        if len(message) > 255:
+            delta = len(message) - 255
+            delta = delta + 3
+            temp_commit_message = str(data['commits'][0]['message'])[:delta]
+            temp_commit_message += "..."
+            commit_message = temp_commit_message
+
+        message = "New commit by %s on %s: %s %s" % (commit_user,
+                                                        repo_name,
+                                                        commit_message,
+                                                        shortened_url)
+
+        print message
+
         return "OK"
 
 if __name__ == '__main__':
